6f70ed1ff2320fc9bf2e15e670c0e6a5daa51a16
jollaman999 <admin@jollaman999.com>
2022-02-12 23:21:01 +0900

    Add Player

--- src/com/jollaman999/bubble/BubbleFrame.java
+++ src/com/jollaman999/bubble/BubbleFrame.java
@@ -1,27 +1,42 @@
 package com.jollaman999.bubble;
 
-import javax.swing.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class BubbleFrame extends JFrame {
-    private JTextField textField;
+    private JLabel backgroundMap;
+    private Player player;
 
     public BubbleFrame() {
-        setSize(1000, 600);
-        getContentPane().setLayout(null);
-
-        JButton btnNewButton = new JButton("New Button");
-        btnNewButton.setBounds(96, 96, 85, 21);
-        getContentPane().add(btnNewButton);
-
-        textField = new JTextField();
-        textField.setText("11111");
-        textField.setBounds(376, 251, 96, 19);
-        getContentPane().add(textField);
-        textField.setColumns(10);
+        initObject();
+        initSetting();
         setVisible(true);
     }
 
     public static void main(String[] args) {
         new BubbleFrame();
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+        setContentPane(backgroundMap); // JPanel 을 JLabel 로 대체함
+
+        player = new Player();
+        add(player);
+//        backgroundMap.setSize(100, 100);
+//        backgroundMap.setLocation(100, 100);
+//        backgroundMap.setSize(1000, 600);
+//        add(backgroundMap); // JFrame 에 JLabel 을 그린다.
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        getContentPane().setLayout(null); // absolute 레이아웃 (자유롭게 그림을 그릴 수 있다.)_
+        setLocationRelativeTo(null); // JFrame 가운데 배치하기
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌때 JVM 같이 종료하기
+        setVisible(true);
+
     }
 }
