10d361886bb48b1c3ad3d64d717c2b6ad642af39
jollaman999 <admin@jollaman999.com>
2022-02-12 12:51:28 +0900

    Initialize Bubble Bubble

---
+++ src/com/jollaman999/bubble/LombokTest.java
@@ -0,0 +1,18 @@
+package com.jollaman999.bubble;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class Dog {
+    private String name;
+}
+
+public class LombokTest {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.setName("토토");
+        System.out.println(d.getName());
+    }
+}
